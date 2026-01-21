729a5493fa7723e3b3e65f13d566e39fc9e92974
Tarun Sai k <tarunsaikolaa@gmail.com>
2026-01-21 10:59:16 +0530

    Update Interviewer.js

--- BACKEND/routes/Interviewer.js
+++ BACKEND/routes/Interviewer.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
+
 const jwt = require("jsonwebtoken");
 const Interviewer = require("../models/Interviewer");
 const Interview = require("../models/Interview");
